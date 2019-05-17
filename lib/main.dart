e83818d54febb4fdfef6dc71672775828809bfd0
SteveOye <seyioyebanjistephen@gmail.com>
2019-05-17 23:14:35 +0100

    control flow

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 void main() {
-  //Learning dart
+  /*
+    Learning dart
+    This is my jornuey through the dart land 
+    Just exploring the basics 
+  */
 
   //in declaring you can use var to assign the type to the 
   //variable name or just use the data type all works 
@@ -57,5 +61,90 @@ void main() {
 
   //Function
   print('\nFunction');
+  String words = 'Every big thing starts small, that the order of life.';
+  toUpperCase(words);
+  toUpperCase('Hello Nigeria');
+
+  //anonymous function
+  var fruitList = ['apples', 'bananas', 'oranges'];
+  fruitList.forEach((item)=> print('${fruitList.indexOf(item)}: $item'));
+
+  //Operators
+  assert(2+3 ==5);
+  assert(2-3 == -1);
+  assert(2*3 == 6);
+  assert(5/2 ==2.5);
+  assert(5~/2 == 2);
+  assert(6%2 == 0);
+  assert('5/2 = ${5 ~/ 2} r ${5%2}' == '5/2 = 2 r 1');
+  // dart support prefix and postfix increment and decrement 
+  var a , b;
+  a= 0;
+  b= ++a;
+  //increment the a before b gets it value making it 1==1
+  assert(a == b);
+
+  a= 0;
+  b= a++;
+  //increment the a after b gets it value making it 1 != 0
+  assert(a != b);
+
+  //Control flow Statement
+  // if and else | for , while and do-while loop | break and continue | switch and case | assert.
+  toUpperCase('\nControl flow Staement');
+  bool isRaining = false;
+  bool isSnowing = true;
+  if(isRaining){
+    print('Rain');
+  }else if(isSnowing){
+    print('snow');
+  }else{
+    print('cool');
+  };
+
+  //for loop
+  //StringBuffer use for concatenation of string
+  var message = StringBuffer('Dart is fun');
+  for (var i = 0; i < 5; i++){
+    message.write('!');
+    print(message);
+  }
+
+  //while and do-while loop
+  // while loop the conition is checked if true the block of code in the body is looped through
+  //if false it passes
+  a = 1;
+  b = a;
+  while(a == b){
+    print('$a , $b');
+    break;
+  }
+
+  //for do-while loop the does the action before checking the condition.
+  /*
+  do{
+    doSomething
+  }while(condition);
+  */
+  //Switch and case
+
+  var command = 'CLOSED';
+  switch (command){
+    case 'OPEN':
+      print('OPEN');
+      break;
+    case 'CLOSED':
+      print('CLOSED');
+      break;
+    default:
+      print('Switch control');
+  }
+
+
 
 }
+
+//this is a function that prints out message in uppercase
+//it has a single parameter of String
+//short form of a funtion using =>
+toUpperCase (String msg)=> print(msg.toUpperCase());
